8afbecf432f4cf520807e14ba3478d50be281e08
Naughty Muppet <naughtymuppet@example.com>
2023-12-02 16:41:20 +0200

    chore: commit compiled files

--- dist/bin/xcom-save-parser.js
+++ dist/bin/xcom-save-parser.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.main = void 0;
@@ -10,7 +11,8 @@ const main = async () => {
     const saveFilePath = (0, node_path_1.join)(process.cwd(), saveFile);
     const data = await (0, promises_1.readFile)(saveFilePath);
     (0, node_fs_1.writeFileSync)(saveFilePath + '.json', JSON.stringify((0, parse_save_1.getSaveInfo)(data), null, 2));
+    console.log('Wrote file to ' + saveFilePath + '.json');
 };
 exports.main = main;
-process.nextTick(exports.main);
+void (0, exports.main)();
 //# sourceMappingURL=xcom-save-parser.js.map
